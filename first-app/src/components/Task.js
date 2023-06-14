68fc6e8ffe1dd0fca6740f935be306d2ce015e7d
Shantanu Shubham <shantanu0707@gmail.com>
2023-06-14 23:33:34 +0530

    [Add] 5.4

---
+++ first-app/src/components/Task.js
@@ -0,0 +1,21 @@
+import { formatDate } from "../utils/DateUtil";
+
+const Task = ({ task: { title, description, createdDate } }) => {
+  return (
+    <div className="card">
+      <div className="content">
+        <div className="header">{title}</div>
+        <div class="meta">{formatDate(createdDate)}</div>
+        <div className="description">{description}</div>
+      </div>
+      <div className="extra content">
+        <div className="ui two buttons">
+          <div className="ui basic green button">Edit</div>
+          <div className="ui basic red button">Delete</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Task;
